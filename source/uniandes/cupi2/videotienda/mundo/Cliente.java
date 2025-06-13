642d474cb16fabacbd5aa0a375372b600feb10df
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-06-12 20:41:17 -0500

    funciones de dar nombre y cedula

---
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -0,0 +1,44 @@
+package uniandes.cupi2.videotienda.mundo;
+
+
+public class Cliente
+{
+
+   //-----------------------------------------------------------------
+   // Atributos
+   //-----------------------------------------------------------------
+
+   private String Cedula;
+   
+   
+   private String Nombre;
+   
+   
+   private String Direccion;
+   
+   
+   private int Saldo;
+
+   //-----------------------------------------------------------------
+   // Constructores
+   
+   
+   public Cliente (String cedula, String nombre, String direccion)
+   {
+	   this.Cedula = cedula;
+	   this.Nombre = nombre;
+	   this.Direccion = direccion;
+	   this.Saldo = 0;
+   }
+   
+   public String darCedula()
+   {
+	   return Cedula;
+   }
+   
+   public String darNombre()
+   {
+	   return Nombre;
+   }
+ 
+}
